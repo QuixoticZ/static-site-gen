3fef13602361e83835b1de7564ee5aa608f42832
QuixoticZ <zwubu@NV-001.>
2025-05-17 21:51:52 -0400

    Added Block type functions

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 
 class BlockType(Enum):
     PARAGRAPH = 1
@@ -9,10 +10,30 @@ class BlockType(Enum):
     ORDERED_LIST = 6
 
 def markdown_to_blocks(markdown):
-    markdown_blocks=markdown.strip().split("\n\n")
-    blocks = [item for item in markdown_blocks if item]
-    return blocks
+    blocks=markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
 
 def block_to_block_type(block):
-    pass
+    if re.match(r"^#{1,6}\s",block):
+        return BlockType.HEADING
+    if re.match(r"^`{3}[\s\S]+`{3}",block):
+        return BlockType.CODE
+    if re.match(r"(?m)^>.+",block):
+        return BlockType.QUOTE
+    if re.match(r"(?m)^- .+",block):
+        return BlockType.UNORDERED_LIST
+    lines=block.split("\n")
+    line_count=1
+    for line in lines:
+        if re.match(rf"^{line_count}\. .+",line):
+            line_count +=1
+    if line_count-1 == len(lines):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
 
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,17 +1,17 @@
 import unittest
 from block_markdown import *
 
-class TestTextNode(unittest.TestCase):
+class TestMarkdownBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
             md = """
-    This is **bolded** paragraph
+This is **bolded** paragraph
 
-    This is another paragraph with _italic_ text and `code` here
-    This is the same paragraph on a new line
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
 
-    - This is a list
-    - with items
-    """
+- This is a list
+- with items
+"""
             blocks = markdown_to_blocks(md)
             self.assertEqual(
                 blocks,
@@ -22,17 +22,57 @@ class TestTextNode(unittest.TestCase):
                 ],
             )
 
-    def assertEqual(self,input1,input2):
-        if input1.__eq__(input2) == True:
-            return True
-        else:
-            return False
+
         
-    def assertNotEqual(self,input1,input2):
-        if input1.__eq__(input2) == True:
-            return False
-        else:
-            return True
+    def test_block_type_heading(self):
+        block="#### TEST"
+        block_result=block_to_block_type(block)
+        
+        self.assertEqual(block_result,BlockType.HEADING)
+
+    def test_block_type_code(self):
+        block="```TEST```"
+        block_result=block_to_block_type(block)
+        self.assertEqual(block_result,BlockType.CODE)
+
+    def test_block_type_quote(self):
+        block="""> test\n> example"""
+        block_result=block_to_block_type(block)
+        self.assertEqual(block_result,BlockType.QUOTE)
+
+    def test_block_type_unordered_list(self):
+        block="""- item1
+- item2
+- item3
+"""
+        block_result=block_to_block_type(block)
+        self.assertEqual(block_result,BlockType.UNORDERED_LIST)
+
+    def test_block_type_ordered_list(self):
+        block="1. item1\n2. item2\n3. item3"
+        block_result=block_to_block_type(block)  
+        self.assertEqual(block_result,BlockType.ORDERED_LIST)
+
+    def test_block_type_paragraph(self):
+        print("Testrunning")
+        block="1 item1\nA item2\n# item"
+        block_result=block_to_block_type(block)
+        self.assertEqual(block_result,BlockType.PARAGRAPH)
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
 
 if __name__ == "__main__":
     unittest.main()
